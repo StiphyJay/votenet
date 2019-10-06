e3b5f40eb297bae2f0f4943617b71a30a74c7133
Hongyuan Du <du-hongyuan@outlook.com>
2019-10-06 10:11:40 -0700

    remove optimizer in eval

--- eval_multi.py
+++ eval_multi.py
@@ -149,16 +149,13 @@ net = Detector(num_class=DATASET_CONFIG.num_class,
 net.to(device)
 criterion = MODEL.get_loss
 
-# Load the Adam optimizer
-optimizer = optim.Adam(net.parameters(), lr=0.001)
-
 # Load checkpoint if there is any
 if CHECKPOINT_PATH is not None and os.path.isfile(CHECKPOINT_PATH):
     checkpoint = torch.load(CHECKPOINT_PATH)
     net.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
-    log_string("Loaded checkpoint %s (epoch: %d)"%(CHECKPOINT_PATH, epoch))
+    best_mAP = checkpoint['mAP']
+    log_string("Loaded checkpoint %s (epoch: %d, best eval mAP@0.5: %f)"%(CHECKPOINT_PATH, epoch, best_mAP))
 
 # Used for AP calculation
 CONFIG_DICT = {'remove_empty_box': (not FLAGS.faster_eval), 'use_3d_nms': FLAGS.use_3d_nms, 'nms_iou': FLAGS.nms_iou,
