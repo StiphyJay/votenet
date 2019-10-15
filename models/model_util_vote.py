b959eacfa43021939d50dfcbd6eb69cc0f73d672
Hongyuan Du <du-hongyuan@outlook.com>
2019-10-15 13:44:22 -0700

    modified a general module for multi-voting

--- models/model_util_vote.py
+++ models/model_util_vote.py
@@ -12,7 +12,6 @@ sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 class VoteConfig(object):
     def __init__(self, 
                  num_heading_bin=4,
-                 normalized_rz=True,
                  max_r=5.75,
                  max_z=1.6,
                  parse_r=F.relu,
@@ -23,9 +22,36 @@ class VoteConfig(object):
         self.num_heading_bin = num_heading_bin
         self.num_spatial_cls = self.num_heading_bin * 2
 
-        self.normalized_rz = normalized_rz
         self.max_r = max_r
         self.max_z = max_z
+        self.max_theta = np.pi / self.num_heading_bin
+        
+        self.parse_r = parse_r
+        self.parse_z = parse_z
+
+        self.top_n_votes = top_n_votes
+        self.best_n_votes = best_n_votes
+
+        assert self.top_n_votes <= self.num_spatial_cls
+
+
+class VoteConfigDistance(object):
+    def __init__(self, 
+                 num_heading_bin=4,
+                 max_r=(2.0, 6.0),
+                 max_z=(1.6,),
+                 parse_r=F.relu,
+                 parse_z=F.relu,
+                 top_n_votes=3,
+                 best_n_votes=768):
+
+        self.num_heading_bin = num_heading_bin
+        self.num_r = len(max_r)
+        self.num_z = len(max_z)
+        self.num_spatial_cls = self.num_r * self.num_z * 2 * self.num_heading_bin
+
+        self.max_r = sorted(max_r)
+        self.max_z = sorted(max_z)
         self.max_theta = np.pi / self.num_heading_bin
         
         self.parse_r = parse_r
--- models/voting_module.py
+++ models/voting_module.py
@@ -79,14 +79,17 @@ class VotingModuleMulti(nn.Module):
         super().__init__()
         self.config = config
         self.num_heading_bin = self.config.num_heading_bin
-        self.num_spatial_cls = self.config.num_spatial_cls
         self.in_dim = seed_feature_dim
+
+        start_theta = torch.arange(self.num_heading_bin, dtype=torch.float32) * (2 * pi) / self.num_heading_bin
+        start_theta = start_theta.repeat(2).view(1, 1, -1)
+        self.register_buffer('start_theta', start_theta)
         
         # TODO: layer parameter
         self.out_vote_dim = 3 + self.in_dim + 1 # (r, z, theta) + (feature) + score
         self.conv1 = torch.nn.Conv1d(self.in_dim, self.in_dim, 1)
         self.conv2 = torch.nn.Conv1d(self.in_dim, self.in_dim, 1)
-        self.conv3 = torch.nn.Conv1d(self.in_dim, self.num_spatial_cls*self.out_vote_dim, 1)
+        self.conv3 = torch.nn.Conv1d(self.in_dim, self.config.num_spatial_cls*self.out_vote_dim, 1)
         self.bn1 = torch.nn.BatchNorm1d(self.in_dim)
         self.bn2 = torch.nn.BatchNorm1d(self.in_dim)
         
@@ -109,22 +112,19 @@ class VotingModuleMulti(nn.Module):
         net = F.relu(self.bn2(self.conv2(net))) 
         net = self.conv3(net) # (batch_size, num_spatial_cls*(3+out_dim+1), num_seed)
                 
-        net = net.transpose(2,1).view(batch_size, num_seed, self.num_spatial_cls, self.out_vote_dim)
+        net = net.transpose(2,1).view(batch_size, num_seed, -1, self.out_vote_dim)
         
         # parse offset
         offset_r = self.config.parse_r(net[:,:,:,0]) # (batch_size, num_seed, num_spatial_cls)
         offset_z = self.config.parse_z(net[:,:,:,1]) # (batch_size, num_seed, num_spatial_cls)
         
-        if self.config.normalized_rz:
-            offset_r *= self.config.max_r
-            offset_z *= self.config.max_z
+        offset_r *= self.config.max_r
+        offset_z *= self.config.max_z
         
         offset_z[:,:,self.num_heading_bin:] *= -1.0
         
         res_theta = net[:,:,:,2] * self.config.max_theta
-        start_theta = torch.arange(self.num_heading_bin, dtype=torch.float32, device=res_theta.device) * (2 * pi) / self.num_heading_bin
-        start_theta = start_theta.repeat(2).view(1, 1, -1)
-        offset_theta = res_theta + start_theta # (batch_size, num_seed, num_spatial_cls)
+        offset_theta = res_theta + self.start_theta # (batch_size, num_seed, num_spatial_cls)
 
         offset_x = offset_r * torch.cos(offset_theta)
         offset_y = offset_r * torch.sin(offset_theta)
@@ -136,6 +136,84 @@ class VotingModuleMulti(nn.Module):
         vote_features = seed_features.transpose(2,1).unsqueeze(2) + residual_features
 
         vote_spatial_score = net[:,:,:,-1] # (batch_size, num_seed, num_spatial_cls)
+        
+        return vote_xyz.contiguous(), vote_features.contiguous(), vote_spatial_score.contiguous()
+
+
+class VotingModuleMultiDistance(nn.Module):
+    def __init__(self, config, seed_feature_dim):
+        """ Votes generation from seed point features.
+
+        Args:
+            config: VoteConfigDistance
+                for testing different configs
+            seed_feature_dim: int
+                number of channels of seed point features
+        """
+        super().__init__()
+        self.config = config
+        self.num_heading_bin = self.config.num_heading_bin
+        self.in_dim = seed_feature_dim
+        
+        # buffer variable
+        self.register_buffer('max_r', torch.tensor(self.config.max_r, dtype=torch.float32).view(1, -1, 1))
+        self.register_buffer('max_z', torch.tensor(self.config.max_z, dtype=torch.float32).view(1, -1, 1))
+        self.register_buffer('start_theta', torch.arange(self.num_heading_bin, dtype=torch.float32) * (2 * pi) / self.num_heading_bin)
+        
+        # TODO: layer parameter
+        self.out_vote_dim = 3 + self.in_dim + 1 # (r, z, theta) + (feature) + score
+        self.conv1 = torch.nn.Conv1d(self.in_dim, self.in_dim, 1)
+        self.conv2 = torch.nn.Conv1d(self.in_dim, self.in_dim, 1)
+        self.conv3 = torch.nn.Conv1d(self.in_dim, self.config.num_spatial_cls*self.out_vote_dim, 1)
+        self.bn1 = torch.nn.BatchNorm1d(self.in_dim)
+        self.bn2 = torch.nn.BatchNorm1d(self.in_dim)
+        
+    def forward(self, seed_xyz, seed_features):
+        """ Forward pass.
+
+        Arguments:
+            seed_xyz: (batch_size, num_seed, 3) Pytorch tensor
+            seed_features: (batch_size, feature_dim, num_seed) Pytorch tensor
+
+        Returns: Note that for the convenience of choosing top n vote, return shape is different
+            vote_xyz: (batch_size, num_seed, num_spatial_cls, 3)
+            vote_features: (batch_size, num_seed, num_spatial_cls, vote_feature_dim)
+            vote_spatial_score: (batch_size, num_seed, num_spatial_cls)
+        """
+        batch_size = seed_xyz.shape[0]
+        num_seed = seed_xyz.shape[1]
+
+        net = F.relu(self.bn1(self.conv1(seed_features))) 
+        net = F.relu(self.bn2(self.conv2(net))) 
+        net = self.conv3(net) # (batch_size, num_spatial_cls*(3+out_dim+1), num_seed)
+                
+        net = net.transpose(2,1).view(batch_size, num_seed, -1, self.out_vote_dim)
+
+        # parse r
+        offset_r = self.config.parse_r(net[..., 0].view(batch_size*num_seed, self.config.num_r, -1))
+        offset_r *= self.max_r
+        offset_r = offset_r.view(batch_size, num_seed, -1, 1)
+
+        #parse z
+        offset_z = self.config.parse_z(net[..., 1].view(-1, self.config.num_z, 2*self.num_heading_bin))
+        offset_z *= self.max_z
+        offset_z[..., self.num_heading_bin:] *= -1.0
+        offset_z = offset_z.view(batch_size, num_seed, -1, 1)
+        
+        res_theta = net[..., 2].view(-1, self.num_heading_bin) * self.config.max_theta
+        offset_theta = res_theta + self.start_theta
+        offset_theta = offset_theta.view(batch_size, num_seed, -1, 1)
+
+        offset_x = offset_r * torch.cos(offset_theta)
+        offset_y = offset_r * torch.sin(offset_theta)
+        offset_xyz = torch.cat((offset_x, offset_y, offset_z), dim=3) # (batch_size, num_seed, num_spatial_cls, 3)
+        
+        vote_xyz = seed_xyz.unsqueeze(2) + offset_xyz # (batch_size, num_seed, num_spatial_cls, 3)
+        
+        residual_features = net[..., 3:-1]
+        vote_features = seed_features.transpose(2,1).unsqueeze(2) + residual_features
+
+        vote_spatial_score = net[..., -1]
         
         return vote_xyz.contiguous(), vote_features.contiguous(), vote_spatial_score.contiguous()
 
